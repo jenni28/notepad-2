c0fdc988b2b068bc690612579946a79c698e179b
Neville Samuell <nevilles@gmail.com>
2012-06-10 00:28:21 -0400

    Tweaking the HomeFragment and NoteFragment UIs

--- gen/com/notepad/R.java
+++ gen/com/notepad/R.java
@@ -14,11 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int deleteButton=0x7f050003;
+        public static final int addNoteButton=0x7f050001;
+        public static final int deleteButton=0x7f050004;
         public static final int homeTitle=0x7f050000;
-        public static final int noteContent=0x7f050004;
-        public static final int noteList=0x7f050001;
-        public static final int noteTitle=0x7f050002;
+        public static final int listView1=0x7f050002;
+        public static final int noteContent=0x7f050005;
+        public static final int noteTitle=0x7f050003;
         public static final int realtabcontent=0x01010000;
     }
     public static final class layout {
@@ -27,9 +28,11 @@ public final class R {
         public static final int note_fragment=0x7f030002;
     }
     public static final class string {
+        public static final int add_note_button_desc=0x7f040004;
         public static final int app_name=0x7f040000;
         public static final int delete_note_button_desc=0x7f040003;
         public static final int home_title=0x7f040001;
-        public static final int note_title=0x7f040002;
+        public static final int note_content_hint=0x7f040005;
+        public static final int note_title_hint=0x7f040002;
     }
 }
